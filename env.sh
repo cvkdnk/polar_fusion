2a54e2e7f02129d9170e2bcd3f4575cc5d77b6d0
cvkdnk <cvkdnk@outlook.com>
2022-11-19 14:30:19 +0800

    1

--- env.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install libsparsehash-dev
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-pip3 install opencv-python
-pip3 install pytorch-lightning
-pip3 install --upgrade git+https://github.com/mit-han-lab/torchsparse.git@v1.4.0
-pip3 install open3d
-pip3 install numba
